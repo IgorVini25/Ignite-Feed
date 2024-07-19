fb3e7e2d71a0703689a46ce50693357d1dca5ead
IgorVini25 <igvinps2006@gmail.com>
2024-07-19 17:15:25 -0300

    ✅⚡️ Comments validity check and clean code improvement

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,41 +6,49 @@ import { Comment } from './Comment'
 import styles from './Post.module.css'
 import { useState } from 'react'
 
-
-export function Post({author, publishedAt, content}) {
-  const [comments, setComments] = useState([
-    'Post muito legal!'
-  ])
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState(['Post muito legal!'])
 
   const [newCommentText, setNewCommentText] = useState('')
 
-  const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm", {
-    locale: ptBR
-  })
+  const publishedDateFormatted = format(
+    publishedAt,
+    "dd 'de' LLLL 'às' HH:mm",
+    {
+      locale: ptBR
+    }
+  )
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
     addSuffix: true
   })
 
-  function handleCreateNewComment(){
+  function handleCreateNewComment() {
     event.preventDefault()
 
     setComments([...comments, newCommentText])
-    setNewCommentText("")
+    setNewCommentText('')
   }
 
-  function handleNewCommentChange(){
+  function handleNewCommentChange() {
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value)
   }
 
-  function deleteComment(commentToDelete){
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório!')
+  }
+
+  function deleteComment(commentToDelete) {
     const commentsWithoutDeletedOne = comments.filter(comment => {
       return comment !== commentToDelete
     })
 
     setComments(commentsWithoutDeletedOne)
   }
+
+  const isNewCommentEmpty = newCommentText.replaceAll(' ', '').length === 0
 
   return (
     <article className={styles.post}>
@@ -54,34 +62,54 @@ export function Post({author, publishedAt, content}) {
           </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
           {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
         {content.map(line => {
-          if(line.type === "paragraph"){
+          if (line.type === 'paragraph') {
             return <p key={line.content}>{line.content}</p>
-          } else if(line.type === "link") {
-            return <p key={line.content}><a href="#">{line.content}</a></p>
+          } else if (line.type === 'link') {
+            return (
+              <p key={line.content}>
+                <a href="#">{line.content}</a>
+              </p>
+            )
           }
         })}
       </div>
 
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu Feedback</strong>
-        <textarea  name='comment' placeholder="Deixe um comentário" value={newCommentText} onChange={handleNewCommentChange} />
+        <textarea
+          name="comment"
+          placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
+        />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
         {comments.map(comment => {
           return (
-            <Comment key={comment} content={comment} onDeleteComment={deleteComment}/>
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
           )
         })}
       </div>
